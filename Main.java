eed3bdf2b5b7b6a7d3f713f9ad370b6df81db361
kiranganthasetti <kiran.gantashetti@cynnent.com>
2025-01-23 14:41:15 +0530

    this is fun

--- Main.java
+++ Main.java
@@ -2,6 +2,6 @@ public class Main
 {
      public static void main(String args[]){
         System.out.println("This is a java Program");
-        system.out.println("This is the output  of the program");
+        System.out.println("This is the output  of the program");
      }
 } 
