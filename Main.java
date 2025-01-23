8aa55b8235a40ce90dad815b39c6ba027164447a
Kiran Gantashetti <95085359+Kiran-gantashetti@users.noreply.github.com>
2025-01-23 14:50:04 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,9 +2,6 @@ public class Main
 {
      public static void main(String args[]){
         System.out.println("This is a java Program");
-<<<<<<< HEAD
         System.out.println("This is the output  of the program");
-        System.out.println("This is the output  of the program");
->>>>>>> a9b95ddbbc987bdee77b00b9422995a583f86c13
      }
 } 
