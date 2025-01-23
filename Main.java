8a5d995cba40dee4d95b2c5722d1f9659594afc4
kiranganthasetti <kiran.gantashetti@cynnent.com>
2025-01-23 13:00:25 +0530

    commiting the changes to print statement

--- Main.java
+++ Main.java
@@ -2,6 +2,6 @@ public class Main
 {
      public static void main(String args[]){
         System.out.println("This is a java Program");
-        system.oout.println("This is the output  of the program");
+        system.out.println("This is the output  of the program");
      }
 } 
