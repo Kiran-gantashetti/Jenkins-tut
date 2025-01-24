bd1d93ce410ca275f070d60338fed23f78ef4973
kiranganthasetti <kiran.gantashetti@cynnent.com>
2025-01-24 15:33:51 +0530

    Integrating a new file

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,5 +3,6 @@ public class Main
      public static void main(String args[]){
         System.out.println("This is a java Program");
         System.out.println("This is the output  of the program");
+        System.out.println("This is endless fun");
      }
 } 
